06eca3e2d9f253b0daa5b61093046e1acb9ebd76
SUHANIS29 <suhanisuryawanshi2935@gmail.com>
2024-05-31 13:57:40 +0530

    Create sqrt.c

---
+++ sqrt.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double a,c;
+    printf("Enter a no.:",a);
+    scanf("%lf",&a);
+    printf("square root of %lf is%lf",a,sqrt(a));
+    return 0;
+}
