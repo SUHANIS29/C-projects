876e654f28f186eacd483923ae7a4299827cc95d
SUHANIS29 <suhanisuryawanshi2935@gmail.com>
2024-05-31 13:52:24 +0530

    Create voteEligibility.c

---
+++ voteEligibility.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a; 
+printf("enter your age:",a);
+scanf("%d",&a);
+if (a>=18) 
+{
+    printf("you are eligible for vote");
+    
+}
+else
+printf("You are not eligible for vote");
+return 0;
+}
+
