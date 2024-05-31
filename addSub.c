40aa46f9c7befc219b16d09f3772384b750393b4
SUHANIS29 <suhanisuryawanshi2935@gmail.com>
2024-05-31 13:56:45 +0530

    Create addSub.c

---
+++ addSub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a , b;
+    printf("Enter a value for a");
+    scanf("%d",&a);
+    printf("Enter a value for b");
+    scanf("%d",&b);
+    printf("the sum is %d\n",a+b);
+    printf("the subtraction is %d\n",a-b);
+    return 0;
+
+}
