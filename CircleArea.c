674d19f62badefc6e01196a05a02e728d771191b
SUHANIS29 <suhanisuryawanshi2935@gmail.com>
2024-05-31 13:51:36 +0530

    Create CircleArea.c

---
+++ CircleArea.c
@@ -0,0 +1,13 @@
+//WAP to write area of circlr
+#include<stdio.h>
+int main()
+{
+  int r;
+  float area;
+  printf("enter value of r");
+  printf("\n");
+  scanf("%d",&r);
+  area=3.142*r*r;
+  printf("Area of circle is%f",area);
+  return 0;
+}
